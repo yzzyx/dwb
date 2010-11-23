bba46980fcb67d7546d62229274ee63fe459f619
portix <portix@gmx.net>
2010-11-23 23:04:53 +0100

    changed comment in formfiller.sh

--- examples/formfiller.sh
+++ examples/formfiller.sh
@@ -4,7 +4,7 @@
 # This script can be used to automatically fill out forms.  The data must be
 # saved in a different file (CONTENT_FILE); each line specifies a form and must
 # be of the form
-# <url> <element_1_name> <element_1_value> <element_2_name> <element_2_name> ... 
+# <url> <element_1_name> <element_1_value> <element_2_name> <element_2_value> ... 
 # example: https://bbs.archlinux.org/login.php req_username username req_password password save_pass true
 
 CONTENT_FILE=${XDG_CONFIG_HOME}/dwb/data/forms
