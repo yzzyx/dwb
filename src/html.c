4e89f5ff3bcc9129980dddd3fe2b513bb4d577e6
portix <portix@gmx.net>
2011-06-23 00:44:50 +0200

    Replace defines by enums, starting branch new_keys

--- src/html.c
+++ src/html.c
@@ -5,6 +5,7 @@
 #define INFO_FILE "info.html"
 #define SETTINGS_FILE "settings.html"
 #define HEAD_FILE "head.html"
+#define KEY_FILE "keys.html"
 
 typedef struct _HtmlTable HtmlTable;
 
@@ -20,11 +21,13 @@ void dwb_html_bookmarks(WebKitWebView *, HtmlTable *);
 void dwb_html_history(WebKitWebView *, HtmlTable *);
 void dwb_html_quickmarks(WebKitWebView *, HtmlTable *);
 void dwb_html_settings(WebKitWebView *, HtmlTable *);
+void dwb_html_keys(WebKitWebView *, HtmlTable *);
 
 static HtmlTable table[] = {
   { "dwb://bookmarks",  "Bookmarks",    INFO_FILE,      0, dwb_html_bookmarks },
   { "dwb://quickmarks", "Quickmarks",   INFO_FILE,      0, dwb_html_quickmarks },
   { "dwb://history",    "History",      INFO_FILE,      0, dwb_html_history },
+  { "dwb://keys",       "Keys",         KEY_FILE,        0, dwb_html_keys },
   //{ "dwb://settings",   "Settings",     SETTINGS_FILE,  0, dwb_html_settings },
 };
 
@@ -71,7 +74,24 @@ dwb_html_history(WebKitWebView *wv, HtmlTable *table) {
 }
 void
 dwb_html_settings(WebKitWebView *wv, HtmlTable *table) {
-  dwb_html_load_page(wv, table, "blub");
+  //dwb_html_load_page(wv, table, "blub");
+}
+void 
+dwb_html_keys_load_cb(WebKitWebView *wv, GParamSpec *p, HtmlTable *table) {
+  PRINT_DEBUG("url: %s", webkit_web_view_get_uri(wv));
+  if (webkit_web_view_get_load_status(wv) == WEBKIT_LOAD_FINISHED) {
+    WebKitDOMDocument *doc = webkit_web_view_get_dom_document(wv);
+    WebKitDOMElement *panel = webkit_dom_document_get_element_by_id(doc, "panel");
+    if (panel)
+      puts(webkit_dom_element_get_attribute(panel, "innerHTML"));
+    g_signal_handlers_disconnect_by_func(wv, dwb_html_keys_load_cb, table);
+  }
+
+}
+void
+dwb_html_keys(WebKitWebView *wv, HtmlTable *table) {
+  g_signal_connect(wv, "notify::load-status", G_CALLBACK(dwb_html_keys_load_cb), table);
+  dwb_html_load_page(wv, table, "blaljlkfds");
 }
 void
 dwb_html_quickmarks(WebKitWebView *wv, HtmlTable *table) {
