c9740243557df550cba0b613c7921d3f080dd0e7
portix <portix@gmx.net>
2011-12-06 16:27:12 +0100

    Clean cache when dwb is closed

--- src/dwb.c
+++ src/dwb.c
@@ -2300,6 +2300,7 @@ dwb_clean_up() {
   if (g_file_test(dwb.files.fifo, G_FILE_TEST_EXISTS)) {
     unlink(dwb.files.fifo);
   }
+  util_rmdir(dwb.files.cachedir, true);
   gtk_widget_destroy(dwb.gui.window);
 #ifdef DWB_ADBLOCKER
   adblock_end();
