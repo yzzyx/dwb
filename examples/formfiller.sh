24c241e836ca4b9085c85894bc6d890642f35b37
portix <portix@gmx.net>
2012-02-12 16:15:54 +0100

    Individually submit forms in formfiller.sh

--- examples/formfiller.sh
+++ examples/formfiller.sh
@@ -1,21 +1,25 @@
 #!/bin/bash 
 # dwb: control mod1 f
-
+ 
 # This script can be used to automatically fill out forms.  The data must be
 # saved in a different file (CONTENT_FILE) each line specifies a form and must
 # be of the form
-# <url> <element_1_name> <element_1_value> <element_2_name> <element_2_value> ... 
-# example: https://bbs.archlinux.org/login.php req_username username req_password password save_pass true
-
+# <url> <submit> <element_1_name> <element_1_value> <element_2_name> <element_2_value> ... 
+# example: https://bbs.archlinux.org/login.php true req_username username req_password password save_pass true
+ 
 CONTENT_FILE=${XDG_CONFIG_HOME}/dwb/data/forms
-SUBMIT=1 
-
-COMMAND+="javascript:function set_value(e, value){if(e.type==\"checkbox\"||e.type==\"radio\"){e.checked=(value.toLowerCase()!=='false'&&value!=='0');}else{e.value=value;}}"
-
+ 
+COMMAND+="exja function set_value(e, value){if(e.type==\"checkbox\"||e.type==\"radio\"){e.checked=(value.toLowerCase()!=='false'&&value!=='0');}else{e.value=value;}}"
+ 
 while read; do
   read -a LINE -d $'\0' <<<"${REPLY}"
-  if [[ ${LINE[0]} = $1 ]]; then 
-    for ((i=1; i<${#LINE[@]}; i++)); do
+  if [[ "$DWB_URI" =~ ${LINE[0]} ]]; then 
+    if [[ ${LINE[1]} = "true" ]]; then
+      SUBMIT=1
+    else
+      SUBMIT=0
+    fi
+    for ((i=2; i<${#LINE[@]}; i++)); do
       NAME=${LINE[$((i++))]}
       VALUE=${LINE[$((i))]}
       COMMAND+="var ${NAME}=document.getElementsByName(\"${NAME}\")[0];set_value(${NAME},\"${VALUE}\");"
@@ -23,6 +27,6 @@ while read; do
     test ${SUBMIT} != 0 && COMMAND+="${NAME}.form.submit();"
   fi
 done < "${CONTENT_FILE}"
-printf "open ${COMMAND}\nclose\n"
-
+printf "${COMMAND}\nclose\n" 
+ 
 # vim: tw=0
