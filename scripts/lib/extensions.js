56554709bc8e39e0053e933c0586324c523f788d
portix <unknown>
2012-05-02 20:06:51 +0200

    Make extension.config private

--- scripts/lib/extensions.js
+++ scripts/lib/extensions.js
@@ -1,7 +1,4 @@
 Object.defineProperties(extensions, { 
-  "config" : {
-    value : undefined
-  }, 
   "warning" : {
     value : function (name, message) {
       io.print("\033[1mDWB EXTENSION WARNING: \033[0mextension  \033[1m" + name + "\033[0m: " + message, "stderr");
@@ -18,51 +15,52 @@ Object.defineProperties(extensions, {
     }
   },
   "load" : {
-    value : function (name) {
-      var boldname = "\033[1m" + name + "\033[0m";
+    value : (function () {
+      var _config = undefined;
+      return function(name) {
+        var boldname = "\033[1m" + name + "\033[0m";
 
-      var config, dataBase, pluginPath, plugin;
-      var extConfig = null;
+        var config, dataBase, pluginPath, plugin;
+        var extConfig = null;
 
-      /* Get default config if the config hasn't been read yet */
-      if (extensions._config === undefined) {
-        try {
-          config = include(data.configDir + "/extensionrc");
+        /* Get default config if the config hasn't been read yet */
+        if (_config === undefined) {
+          try {
+            config = include(data.configDir + "/extensionrc");
+          }
+          catch (e) {
+            extensions.error(name, "loading config failed : " + e);
+          }
+          if (config === null) {
+            extensions.warning(name, "Could not load config.");
+          }
+          else {
+            _config = config;
+          }
         }
-        catch (e) {
-          extensions.error(name, "loading config failed : " + e);
+        if (_config) {
+          extConfig = _config[name] || null;
         }
-        if (config === null) {
-          extensions.warning(name, "Could not load config.");
+
+        /* Load extension */
+        plugin = include(data.userDataDir + "/extensions/" + name);
+        if (plugin === null) {
+          plugin = include(data.systemDataDir + "/extensions/" + name);
+          if (plugin === null) {
+            extensions.error(name, "Couldn't find extension.");
+            return false;
+          }
+
+        }
+        if (plugin.init(extConfig)) {
+          extensions.message(name, "Successfully loaded and initialized.");
+          return true;
         }
         else {
-          Object.defineProperty(extensions, "_config", { 
-            value : config
-          });
-        }
-      }
-      if (extensions._config) {
-        extConfig = extensions._config[name] || null;
-      }
-
-      /* Load extension */
-      plugin = include(data.userDataDir + "/extensions/" + name);
-      if (plugin === null) {
-        plugin = include(data.systemDataDir + "/extensions/" + name);
-        if (plugin === null) {
-          extensions.error(name, "Couldn't find extension.");
+          extensions.error(name, "Initialization failed.");
           return false;
         }
-
-      }
-      if (plugin.init(extConfig)) {
-        extensions.message(name, "Successfully loaded and initialized.");
-        return true;
-      }
-      else {
-        extensions.error(name, "Initialization failed.");
-        return false;
-      }
-    }
+      };
+    })()
   }
 });
