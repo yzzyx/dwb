528e2ddd3cd08c359fcf1e4c922ae1b7f162f8d8
portix <unknown>
2012-06-02 16:55:27 +0200

    Execute binded javascript functions with POST_SM

--- src/scripts.c
+++ src/scripts.c
@@ -520,7 +520,7 @@ global_bind(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size
   Key key = dwb_str_to_key(keystr);
   map->key = key.str;
   map->mod = key.mod;
-  FunctionMap fm = { { NULL, NULL }, CP_DONT_SAVE, (Func)scripts_eval_key, NULL, ALWAYS_SM, { .p = func, .ro = true }, EP_NONE,  {NULL} };
+  FunctionMap fm = { { NULL, NULL }, CP_DONT_SAVE, (Func)scripts_eval_key, NULL, POST_SM, { .p = func, .ro = true }, EP_NONE,  {NULL} };
   *fmap = fm;
   map->map = fmap;
   dwb.keymap = g_list_prepend(dwb.keymap, map);
