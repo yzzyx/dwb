8dc91e992f4081c7a2f61efc73bb92d427b5edb1
portix <unknown>
2012-06-09 10:44:26 +0200

    Adding googledocs.js to examples

---
+++ examples/js/googledocs.js
@@ -0,0 +1,63 @@
+#!javascript
+
+/*
+ * Shortcut to toggle usage of Google Docs
+ */
+var shortCut = "tgd";
+
+/*
+ * Whether to initially use Google Docs
+ */
+var useDocs = true;
+
+/*
+ * list off all supported filetypes, comment / uncomment to enable/disable
+ * filetype
+ */
+var supported = [ 
+  "DOC", 
+  "DOCX", 
+  "XLS", 
+  "XLSX", 
+  "PPT", 
+  "PPTX", 
+  "ODT", 
+  "ODS",
+  "PDF", 
+  "PAGES", 
+  "AI", 
+  "PSD", 
+  "TIFF", 
+  "DXF", 
+  "SVG", 
+  "EPS", 
+  "PS", 
+  "TTF",
+  "OTF", 
+  "XPS", 
+  // "ZIP", 
+  // "RAR"  
+];
+
+
+var id = signals.connect("download", downloadCheck);
+var reg = new RegExp(".*\.(" + supported.join("|") + ")$", "i");
+
+function downloadCheck(w, d) {
+  if (reg.test(d.networkRequest.uri)) {
+    w.loadUri("http://docs.google.com/viewer?url=" + d.networkRequest.uri);
+    return true;
+  }
+}
+function toggleDocs() {
+  useDocs = !useDocs;
+  if (!useDocs) {
+    signals.disconnect(id);
+    io.notify("Google Docs disabled");
+  }
+  else {
+    id = signals.connect("download", downloadCheck);
+    io.notify("Google Docs enabled");
+  }
+}
+bind(shortCut, toggleDocs, "googleDocs");
