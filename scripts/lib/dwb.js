5365773bdd681808346c7a8184b45e46f6911dce
portix <unknown>
2012-10-31 22:07:30 +0100

    Return all modules if array is null in require

--- scripts/lib/dwb.js
+++ scripts/lib/dwb.js
@@ -4,44 +4,47 @@
     var _callbacks = [];
     var _initialized = false;
     var _applyRequired = function(names, callback) {
-      var modules = [];
-      for (var j=0, m=names.length; j<m; j++) {
-        modules.push(_modules[names[j]]);
+      if (names == null) 
+        callback.call(this, _modules);
+      else {
+        var modules = [];
+        for (var j=0, m=names.length; j<m; j++) {
+          modules.push(_modules[names[j]]);
+        }
+        callback.apply(this, modules);
       }
-      callback.apply(this, modules);
     };
     Object.defineProperties(this, { 
-        provide : { 
+        "provide" : { 
           value : function(name, module) {
-            if (_modules[name]) {
+            if (_modules[name]) 
               io.debug("provide: Module " + name + " is already defined!");
-            }
             else 
               _modules[name] = module;
           }
         },
-        require : {
+        "require" : {
           value : function(names, callback) {
-            if (!_initialized) {
+            io.print("apply");
+            if (!_initialized) 
               _callbacks.push({callback : callback, names : names});
-            }
-            else {
+            else 
               _applyRequired(names, callback);
-            }
           }
         },
-        // Immediately deleted from the global object
+        // Immediately deleted from the global object, so it is not callable
+        // from other scripts
         _init : { 
           value : function() {
             _initialized = true;
             var names, callback, modules, name;
-            for (var i=0, l=_callbacks.length; i<l; i++) {
+            for (var i=0, l=_callbacks.length; i<l; i++) 
               _applyRequired(_callbacks[i].names, _callbacks[i].callback);
-            }
+
             Object.freeze(this);
-        },
-        configurable : true
-      }
-  });
+          },
+          configurable : true
+        }
+    });
 })();
 Object.preventExtensions(this);
